79b603b3cf06bb17b69730d5b4650fc91c164135
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2013-02-27 23:39:41 -0500

    Changed emdash character entity to UTF-8.
    Trying to get this file pushed to babbage, actually.

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php
   date_default_timezone_set('America/New_York');
   set_include_path('includes');
-	require_once('markdown.php');
-	
-	function scandir_r($dir)
-	{
-		$files = array();
-		$nodes = scandir($dir);
-		foreach ($nodes as $node)
-		{
-			if (is_dir("$dir/$node"))
-			{
-				if ($node[0] !== '.' && $node[0] !== '_')
-			  {
-				  $files = $files + scandir_r("$dir/$node");
-				}
-			}
-			else
-			{
-				$files["$dir/$node"] = filemtime("$dir/$node");
-			}
-		}
-		return $files;
-	}
+  require_once('markdown.php');
+
+  function scandir_r($dir)
+  {
+    $files = array();
+    $nodes = scandir($dir);
+    foreach ($nodes as $node)
+    {
+      if (is_dir("$dir/$node"))
+      {
+        if ($node[0] !== '.' && $node[0] !== '_')
+        {
+          $files = $files + scandir_r("$dir/$node");
+        }
+      }
+      else
+      {
+        $files["$dir/$node"] = filemtime("$dir/$node");
+      }
+    }
+    return $files;
+  }
 
   $mime_type = "text/html";
   $html_attributes="lang=\"en\"";
@@ -51,59 +51,59 @@
     <link rel='stylesheet' href="./Assignment_03/css/assignment_03.css"/>
   </head>
   <body>
-  	<h1>The Professor’s Assignments</h1>
+    <h1>The Professor’s Assignments</h1>
     <?php
-		$dir = opendir('.') or die("<h2 class='error'>Error: unable to open directory</h2>" .
-		    "</body></html>\n");
-		while ($dir_path = readdir($dir))
-		{
-			if (is_dir($dir_path) && $dir_path[0] !== '.' && $dir_path[0] !== '_')
-			{
-				$files = scandir_r($dir_path);
-				$max_file = 'Unknown';
-				$max_time = 0;
-				foreach($files as $file => $mtime)
-				{
-					if ($mtime > $max_time)
-					{
-						$max_time = $mtime;
-						$max_file = $file;
-					}
-				}
-				if ($max_time === 0)
-				{
-					$max_file = 'Empty Directory';
-					$max_time = filemtime($dir_path);
-				}
-				else
-				{
-					$max_file = substr($max_file, strlen($dir_path) + 1);
-				}
-				$modified_str = date('F j, Y \a\t g:i a', $max_time);
-			  echo <<<EOD
+    $dir = opendir('.') or die("<h2 class='error'>Error: unable to open directory</h2>" .
+        "</body></html>\n");
+    while ($dir_path = readdir($dir))
+    {
+      if (is_dir($dir_path) && $dir_path[0] !== '.' && $dir_path[0] !== '_')
+      {
+        $files = scandir_r($dir_path);
+        $max_file = 'Unknown';
+        $max_time = 0;
+        foreach($files as $file => $mtime)
+        {
+          if ($mtime > $max_time)
+          {
+            $max_time = $mtime;
+            $max_file = $file;
+          }
+        }
+        if ($max_time === 0)
+        {
+          $max_file = 'Empty Directory';
+          $max_time = filemtime($dir_path);
+        }
+        else
+        {
+          $max_file = substr($max_file, strlen($dir_path) + 1);
+        }
+        $modified_str = date('F j, Y \a\t g:i a', $max_time);
+        echo <<<EOD
       <h2>
-			  <a href='$dir_path'>$dir_path</a>: last modified $modified_str ($max_file)
-		  </h2>
+        <a href='$dir_path'>$dir_path</a>: last modified $modified_str ($max_file)
+      </h2>
 
 EOD;
-				//	Find a readme file to display, if there is one
-				$readme_contents = "<h3 class='error'>Missing README file!</h3>\n";
-				$readme_files = array('README.md', 'README', 'README.txt');
-				foreach ($readme_files as $readme_file)
-				{
-				  if (is_file("$dir_path/$readme_file"))
-				  {
-				     $readme_contents = Markdown(file_get_contents("$dir_path/$readme_file"));
-						 break;
-				  }
-				}
-			  echo "<div class='readme'>$readme_contents</div>\n";
-			}
-		}
-		?>
+        //  Find a readme file to display, if there is one
+        $readme_contents = "<h3 class='error'>Missing README file!</h3>\n";
+        $readme_files = array('README.md', 'README', 'README.txt');
+        foreach ($readme_files as $readme_file)
+        {
+          if (is_file("$dir_path/$readme_file"))
+          {
+             $readme_contents = Markdown(file_get_contents("$dir_path/$readme_file"));
+             break;
+          }
+        }
+        echo "<div class='readme'>$readme_contents</div>\n";
+      }
+    }
+    ?>
     <footer>
       <a href="http://validator.w3.org/check?uri=referer">XHTML</a>
-      &#x2014;
+      —
       <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3">CSS</a>
     </footer>
   </body>
