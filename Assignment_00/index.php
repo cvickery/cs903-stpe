9b3c49a391d0a8efbdbc11e24361ca2372cb9086
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2013-02-28 16:44:26 -0500

    Added stylesheet for Assignment 0.

--- Assignment_00/index.php
+++ Assignment_00/index.php
@@ -1,11 +1,15 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?php
+  header('Content-type: application/xhtml+xml');
+  echo "<?xml version='1.0' encoding='UTF-8'?>\n";
+?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
   <head>
     <title>My Zeroth Assignment</title>
+    <link rel='stylesheet' type='text/css' href='css/assignment_0.css' />
   </head>
   <body>
-    <h1>My Zeroth Assignment</h2>
+    <h1>My Zeroth Assignment</h1>
 <?php
   $dir = opendir('..');
   while ($node = readdir($dir))
@@ -19,4 +23,4 @@
       <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3">CSS</a>
     </footer>
   </body
-</html>
+></html>
