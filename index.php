b76203667082fcaa9e759c63f67dfba073cf1a63
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2013-03-07 23:18:41 -0500

    Working on error reporting.

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+  ini_set('error_reporting', E_ALL);
+  ini_set('display_errors', '1');
+  ini_set('log_errors', '~/stpe9999/htdocs/error_log');
   date_default_timezone_set('America/New_York');
   set_include_path('includes');
   require_once('markdown.php');
