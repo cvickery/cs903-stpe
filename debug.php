4066a5f1c9de335c7d66d83e15a57826a5b72a8b
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2013-03-12 15:34:25 -0400

    Debug debug.php

--- debug.php
+++ debug.php
@@ -1,6 +1,7 @@
 <?php
   ini_set('error_reporting', E_ALL);
   ini_set('display_errors', '1');
-  ini_set('log_errors', '~stpe9999/htdocs/error_log');
+  ini_set('log_errors',
+      '/Volumes/Sensitive/Library/WebServer/Documents/babbage.cs.qc.cuny.edu/cs903/stpe9999/htdocs/error_log');
   include('index.php');
 ?>
