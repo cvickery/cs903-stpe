a4d93dcf005625054e7f2fbcd2326897f6d03ad8
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2013-03-12 15:41:58 -0400

    Debug debug.php

--- debug.php
+++ debug.php
@@ -1,4 +1,5 @@
 <?php
+  date_default_timezone_set('America/New_York');
   ini_set('error_reporting', E_ALL);
   ini_set('display_errors', '1');
   ini_set('log_errors', '~stpe9999/htdocs/error_log');
