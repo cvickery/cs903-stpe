fb72e472aba712ae93df3be481d7f6a9250d0da8
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2013-03-15 18:56:51 -0400

    Mechanism to clear error log. Reorder debug.php to allow both display and logging.

--- debug.php
+++ debug.php
@@ -1,10 +1,9 @@
 <?php
   date_default_timezone_set('America/New_York');
   ini_set('error_reporting', E_ALL);
-  ini_set('display_errors', 'Off');
   ini_set('Log_errors', 'On');
-  ini_set('ignore_repeated_errors', 'On');
   ini_set('error_log', './error_log');
+  ini_set('display_errors', 'On');
   assert_options(ASSERT_ACTIVE, 1);
   assert_options(ASSERT_WARNING, 1);
   include('index.php');
